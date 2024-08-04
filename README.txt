24ab6ab5503227c0f09628e92bc5fcdcdba6a661
Đào Đức Lam <daoduclam0620@gmail.com>
2024-08-04 11:43:44 +0700

    fsasa

---
+++ README.txt
@@ -0,0 +1,29 @@
+Current body font: font-family:"Heebo",sans-serif; (change in bootstrap css)
+Current header font: font-family:"Nunito",sans-serif
+Current color code: :
+    --primary: #06BBCC;
+    --light: #F0FBFC;
+    --dark: #181d38;
+Icons: Fontawesome
+Email: web3forms
+
+
+------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------
+****README****
+*** Start date (30/5/24), end date (9/7/24)
+*** People involved:Tran Dinh Hai; Luong Chi Bach; Dao Duc Lam
+*** Software and versions: LuyenThi123 website 1.0
+*** Data:12.5 MB
+*** Language Usage: HTML;CSS;Javascript;Bootstrap;PHP
+*** Disclaimer, copyright & licencing: This is a project of RMIT Techies Club. All materials and content provided on this website are for educational and 
+informational purposes only. The RMIT Techies Club does not guarantee the accuracy, completeness, or usefulness of any information provided. The content and 
+resources are provided "as is" without any warranties of any kind, either express or implied. All content, including text, graphics, logos, and images, is 
+the property of the RMIT Techies Club and is protected by copyright laws. Unauthorized use and/or duplication of this material without express and 
+written permission from the RMIT Techies Club is strictly prohibited.
+This project is licensed under the MIT License, which allows for reuse, modification, and distribution of the code. For more details, see the LICENSE file 
+included in the repository. By using this website, you agree to these terms and conditions. If you have any questions or need further information, please 
+contact us at 
+
+
+
---
+++ js/main.js
@@ -0,0 +1,180 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').css('top', '0px');
+        } else {
+            $('.sticky-top').css('top', '-100px');
+        }
+    });
+    
+    
+    // Dropdown on mouse hover
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+    
+    $(window).on("load resize", function() {
+        if (this.matchMedia("(min-width: 992px)").matches) {
+            $dropdown.hover(
+            function() {
+                const $this = $(this);
+                $this.addClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "true");
+                $this.find($dropdownMenu).addClass(showClass);
+            },
+            function() {
+                const $this = $(this);
+                $this.removeClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "false");
+                $this.find($dropdownMenu).removeClass(showClass);
+            }
+            );
+        } else {
+            $dropdown.off("mouseenter mouseleave");
+        }
+    });
+    // Document Sliders
+    document.addEventListener('DOMContentLoaded', function () {
+        function initializePagination(sectionId, prevBtnId, nextBtnId) {
+            let currentPage = 1;
+            const pages = document.querySelectorAll(`#${sectionId} .document-page`);
+            const totalPages = pages.length;
+
+            function showPage(page) {
+                pages.forEach((el, index) => {
+                    el.classList.toggle('active', index + 1 === page);
+                });
+            }
+
+            document.getElementById(nextBtnId).addEventListener('click', function () {
+                if (currentPage < totalPages) {
+                    currentPage++;
+                    showPage(currentPage);
+                }
+            });
+
+            document.getElementById(prevBtnId).addEventListener('click', function () {
+                if (currentPage > 1) {
+                    currentPage--;
+                    showPage(currentPage);
+                }
+            });
+
+            // Show the first page initially
+            showPage(currentPage);
+        }
+
+        initializePagination('math-documents', 'prev-math', 'next-math');
+        initializePagination('ielts-documents', 'prev-ielts', 'next-ielts');
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+})(jQuery);
+
+
+function showPage(page) {
+    const pages = document.querySelectorAll('.job-page');
+    pages.forEach(pageElement => {
+        pageElement.style.display = 'none';
+    });
+    const currentPageJobs = document.querySelectorAll(`.page-${page}`);
+    currentPageJobs.forEach(job => {
+        job.style.display = 'table-row';
+    });
+}
+
+// Show the first page by default
+showPage(1);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const anchors = document.querySelectorAll('nav a.center-scroll');
+
+    anchors.forEach(anchor => {
+        anchor.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            const targetId = anchor.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                const offset = window.innerHeight / 2 - targetElement.clientHeight / 2;
+                window.scrollTo({
+                    top: targetElement.offsetTop - offset,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
+
